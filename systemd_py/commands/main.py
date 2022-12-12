bd5b551a26b744c83df610814463623bd1177e13
amiwrpremium <amiwrpremium@gmail.com>
2022-12-12 17:54:16 +0330

    cli tools

--- systemd_py/commands/main.py
+++ systemd_py/commands/main.py
@@ -1,6 +1,7 @@
 from typer import Typer
 
 from .interactive.main import app as interactive_app
+from .cli.main import app as cli_app
 
 app = Typer(
     name="systemd-py",
@@ -8,6 +9,7 @@ app = Typer(
 )
 
 app.add_typer(interactive_app, name="interactive")
+app.add_typer(cli_app, name="cli")
 
 if __name__ == "__main__":
     app()
